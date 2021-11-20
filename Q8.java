1bb0aa34e93a6211eb87211db827d05883620176
Aishikboy109 <ujanaishik109@gmail.com>
2021-11-20 23:18:35 +0530

    commit 2

--- Q8.java
+++ Q8.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Q8 {
-    
+    //Remove duplicate characters from a word
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //user input
+        System.out.println("Enter the word : ");
+        String word = sc.next();
+        //Printing out the sanitized word
+        System.out.println("Sanitized word : " + removeDuplicateChars(word));
+    }
+
+    static String removeDuplicateChars(String word){
+        //Recursive Function for removing duplicate characters
+       if(word.length()<=1){
+           return word;
+       }
+       else if(word.charAt(0) == word.charAt(1)){
+            return removeDuplicateChars(word.substring(1));
+       }
+       else{
+           return word.charAt(0) + removeDuplicateChars(word.substring(1));
+       }
+    }
 }
---
+++ Q9.java
@@ -0,0 +1,7 @@
+public class Q9 {
+    //Java Program to print spiral matrix
+
+    public static void main(String[] args) {
+        
+    }
+}
